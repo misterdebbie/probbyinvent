942d1439ee4d77f62750424f186ee2637bc7d956
debbie chong <giyoungchong1980@gmail.com>
2018-04-20 19:44:51 -0400

    Add event listener ES6 style

--- click.js
+++ click.js
@@ -1,17 +1,41 @@
 console.log('nugget');
-let puppyEl = document.getElementById('puppyTarget');
-let total = 0;
-/*puppyEl.addEventListener('click', function(){
-  console.log('you clicked the puppy');
-  total += 1;
-  console.log(total);
-}, false);*/
+let colaEl = document.getElementById('colaTarget');
+let colaTotal = 0;
 
-puppyEl.addEventListener('click', () => {
-  console.log('you clicked the puppy!');
-  total += 1;
-  console.log(total);
-  if (total > 5){
-    console.log('you clicked ' + total + ' times. STOP');
+let colaObj = {
+  name: 'cola fresh'
+};
+let updateName = document.getElementById('colaName');
+updateName.textContent = colaObj.name;
+
+colaEl.addEventListener('click', () => {
+  console.log('you clicked the cola!');
+  colaTotal += 1;
+  console.log(colaTotal);
+  if (colaTotal > 5){
+    console.log('you clicked ' + colaTotal + ' times. STOP');
   }
+  let totalColaClicks = document.getElementById('colaClicks');
+  totalColaClicks.textContent = colaTotal;
+}, false);
+
+let burgerEl = document.getElementById('burgerTarget');
+let burgerTotal = 0;
+
+let burgerObj = {
+  name: 'cheeseburgerrrr'
+};
+
+let updateBurger = document.getElementById('burgerName');
+updateBurger.textContent = burgerObj.name;
+
+burgerEl.addEventListener('click', () => {
+  console.log('you clicked the burgerrrrr!');
+  burgerTotal += 1;
+  console.log(burgerTotal);
+  if (burgerTotal > 3){
+    console.log('you clicked ' + burgerTotal + ' times. STOP');
+  }
+  let totalBurgerClicks = document.getElementById('burgerClicks');
+  totalBurgerClicks.textContent = burgerTotal;
 }, false);
